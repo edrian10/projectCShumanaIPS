e4d8bfedb2ab423bc7140a366710d3033fb14dcf
EDRIAN ESTRADA <edraestrada@misena.edu.co>
2022-10-22 21:38:15 -0500

    pagina de index completa sin errores empezar las siguientes en ramas no alterar el main

--- base/settings.py
+++ base/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'America Bogota'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
