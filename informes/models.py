94e09dd423fc00aa70fc4bf5210dd7d8d8f6ca0b
EDRIAN ESTRADA <edraestrada@misena.edu.co>
2022-10-25 16:47:16 -0500

    Se eliminaros, los informes y detalles de informes

--- informes/models.py
+++ informes/models.py
@@ -4,27 +4,15 @@ from usuarios.models import Medico, Pacientes
 
 # Create your models here
 # TABLA DE HISTORIA CLÍNICA.
-class HistoriaClinica(models.model):
-    fechaAtencion=models.DateField(_max_length=20, verbose_name="Fecha de Atención")
-    motivoConsulta=models.CharField(_max_length=45, verbose_name="Motivo de la Consulta")
-    causasExternas=models.CharField(_max_length=45, verbose_name="Causas Externas")
-    alergiaMedicamento=models.CharField(_max_length=245, verbose_name="Alergia a Medicamento")
-    cirugiasRecibidas=models.CharField(_max_length=245, verbose_name="Cirugias recibidas")
-    tratamientosMedicamentos=models.CharField(_max_length=245, verbose_name="Tratamiento o Medicamentos")
-    informeYtratamientos=models.CharField(_max_length=245, verbose_name="Informe Medico y tratamientos")
-    nombresAcompañante=models.CharField(_max_length=45, verbose_name="Nombres del Acompañante")
-    telAcompañante=models.CharField(_max_length=45, verbose_name="Teléfono del Acompañante")
+class HistoriaClinica(models.Model):
+    fechaAtencion=models.DateField(max_length=20, verbose_name="Fecha de Atención")
+    motivoConsulta=models.CharField(max_length=45, verbose_name="Motivo de la Consulta")
+    causasExternas=models.CharField(max_length=45, verbose_name="Causas Externas")
+    alergiaMedicamento=models.CharField(max_length=245, verbose_name="Alergia a Medicamento")
+    cirugiasRecibidas=models.CharField(max_length=245, verbose_name="Cirugias recibidas")
+    tratamientosMedicamentos=models.CharField(max_length=245, verbose_name="Tratamiento o Medicamentos")
+    informeYtratamientos=models.CharField(max_length=245, verbose_name="Informe Medico y tratamientos")
+    nombresAcompañante=models.CharField(max_length=45, verbose_name="Nombres del Acompañante")
+    telAcompañante=models.CharField(max_length=45, verbose_name="Teléfono del Acompañante")
     pacientes=models.ForeignKey(Pacientes, on_delete=models.CASCADE, verbose_name="Paciente")
     medico=models.ForeignKey(Medico, on_delete=models.CASCADE, verbose_name="Medico")
-
-class Informe(models.model):
-    cantidadDeServiosDia=models.CharField(_max_length=20, verbose_name="Fecha de Atención")
-    fechaGeneracionInforme=models.DateField(_max_length=20, verbose_name="Fecha de Atención")
-    fechaInicioInforme=models.DateField(_max_length=20, verbose_name="Fecha de Atención")
-    fechaFinInforme=models.DateField(_max_length=20, verbose_name="Fecha de Atención")
-
-# TABLA DE DETALLE INFORME.
-class DetalleInforme(models.model):
-    informe=models.ForeignKey(Informe, on_delete=models.CASCADE, verbose_name="Informe")
-    historiaClinica=models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE, verbose_name="Historia Clínica")
-    
--- usuarios/models.py
+++ usuarios/models.py
@@ -3,25 +3,27 @@ from django.db import models
 from django.forms import CharField
 
 from informes.models import Informe
-from.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
+from lugares.models import Barrio
 
 #TABLA ESPECIALIDAD.
-class Especialidad(models.model):
+class Especialidad(models.Model):
     nombresEspecialidad=models.CharField(_max_length=45, verbose_name="Nombres de la Especialidad")
-    class Estado(models.model):
+    class Estado(models.Model):
         Activo='1',_('Activo')
         Inactivo='0',_('Inactivo')
     estado=models.CharField(max_length=2, choices=Estado.choices, default=Estado.Activo, verbose_name="Estado")
 
 
 #TABLA MEDICO
-class Medico(models.model):
+class Medico(models.Model):
     codigoMedico=models.CharField(_max_length=45, verbose_name="Código de medico")
-    class TipoDocumento(models.model):
+    class TipoDocumento(models.Model):
         CC='CC', _('Cédula de Ciudadanía')
         TI='TI', _('Tarjeta de Identidad')
         RC='RC', _('Registro Civil')
-        PP='PP', _('Pasaporte')
+        PP='PP', _('Pasaporte.model)te')
         CE='CE', _('Cédula de Extranjería')
         OT='Otros',_('Otro Tipo de Identidad')
     tipoDocumento=models,CharField(max_length=6, choices=TipoDocumento.choices, default=TipoDocumento.CC, verbose_name="Tipo de Documento")
@@ -32,7 +34,7 @@ class Medico(models.model):
     email=models.CharField(max_length=45, verbose_name="Email")
     cantServDia=models.CharField(_max_length=45, verbose_name="Cantidad de servicios al día")
     cantServMes=models.CharField(_max_length=45, verbose_name="Cantidad de servicios al Mes")
-    class Estado(models.model):
+    class Estado(models.Model):
         Activo='1',_('Activo')
         Inactivo='0',_('Inactivo')
     estado=models.CharField(max_length=2, choices=Estado.choices, default=Estado.Activo, verbose_name="Estado")
@@ -40,11 +42,11 @@ class Medico(models.model):
     Especialidad=models.ForeignKey(Especialidad, on_delete=models.CASCADE, verbose_name="Especialidad")
 
 #TABLA PACIENTES
-class Pacientes(models.model):
+class Pacientes(models.Model):
     nombres=models.CharField(_max_length=45, verbose_name="Nombres")
     apellidos=models.CharField(_max_length=45, verbose_name="Apellidos")
     edad=models.DateField(_max_length=45, verbose_name="Edad")
-    class TipoDocumento(models.model):
+    class TipoDocumento(models.Model):
         CC='CC', _('Cédula de Ciudadanía')
         TI='TI', _('Tarjeta de Identidad')
         RC='RC', _('Registro Civil')
@@ -53,12 +55,12 @@ class Pacientes(models.model):
         OT='Otros',_('Otro Tipo de Identidad')
     tipoDocumento=models,CharField(max_length=6, choices=TipoDocumento.choices, default=TipoDocumento.CC, verbose_name="Tipo de Documento")
     numeroDocumento=models.CharField(max_length=50, verbose_name="Número de documento")
-    class Genero(models.model):
+    class Genero(models.Model):
         F='F',_('Femenino')
         M='M',_('Masculino')
         OT='OTRO',_('Otro')
     genero=models.CharField(max_length=4, choices=Genero.choices, default=Genero.OTROS, verbose_name="Genero")
-    class GrupoSanguineo(models.model):
+    class GrupoSanguineo(models.Model):
         A_POSITIVO ='A+', _('A+')
         A_NEGATIVO='A-', _('A-')
         B_POSITIVO='B+', _('B+')
@@ -71,7 +73,7 @@ class Pacientes(models.model):
     direccion=models.CharField(max_length=45, verbose_name="Dirección")
     email=models.CharField(max_length=45, verbose_name="Email")
     telefono=models.CharField(max_length=20, verbose_name="Número de Teléfono")
-    class EstadoCivil(models.model):
+    class EstadoCivil(models.Model):
         Casado ='Casado', _('Casado')
         Divorciado ='Divorciado', _('Divorciado')
         Soltero ='Soltero', _('Soltero')
@@ -79,26 +81,23 @@ class Pacientes(models.model):
         Otro ='Otro', _('Otro')
     estadoCivil=models,CharField(max_length=10, choices=EstadoCivil.choices, default=EstadoCivil.Otro, verbose_name="Estado Civil")
     ocupacion=models.CharField(_max_length=100, verbose_name="Ocupación")
-    class RegimenSalud(models.model):
+    class RegimenSalud(models.Model):
         subsidiado='subsidiado',_('subsidiado')
         contributivo='contributivo',_('contributivo')
         OT='OTRO', _('Otro')
     genero=models.CharField(max_length=4, choices=Genero.choices, default=Genero.OTRO, verbose_name="Genero")
-    class Estado(models.model):
+    class Estado(models.Model):
         Activo='1',_('Activo')
         Inactivo='0',_('Inactivo')
     estado=models.CharField(max_length=2, choices=Estado.choices, default=Estado.Activo, verbose_name="Estado")
     EPS=models.ForeignKey(EPS, on_delete=models.CASCADE, verbose_name="Paciente")
     barrio=models.ForeignKey(Barrio, on_delete=models.CASCADE, verbose_name="Barrio")
-    #Falta configurar las llaves foráneas de eps y barrio no se si van en este usuarios.
-
 
 #TABLA USUARIOS
-
-class Usuarios(models.model):
+class Usuarios(models.Model):
     nombres=models.CharField(_max_length=45, verbose_name="Nombres")
     apellidos=models.CharField(_max_length=45, verbose_name="Apellidos")
-    class TipoDocumento(models.model):
+    class TipoDocumento(models.Model):
         CC='CC', _('Cédula de Ciudadanía')
         TI='TI', _('Tarjeta de Identidad')
         RC='RC', _('Registro Civil')
@@ -109,16 +108,12 @@ class Usuarios(models.model):
     numeroDocumento=models.CharField(max_length=50, verbose_name="Número de documento")
     telefono=models.CharField(max_length=20, verbose_name="Número de Teléfono")
     email=models.CharField(max_length=100, verbose_name="email")
-    class TipoUsuario(models.model):
+    class TipoUsuario(models.Model):
         Administrador ='Administrador', _('Administrador')
         Recepcionista='Recepcionista', _('Recepcionista')
     tipoUsuario=models.CharField(max_length=4, choices=TipoUsuario.choices, default=TipoUsuario.OTROS, verbose_name="Tipo de Usuario")
-    class Estado(models.model):
+    class Estado(models.Model):
         Activo='1',_('Activo')
         Inactivo='0',_('Inactivo')
     estado=models.CharField(max_length=2, choices=Estado.choices, defaulT=Estado.Activo, verbose_name="Estado")
-    pacientes=models.ForeignKey(Pacientes, on_delete=models.CASCADE, verbose_name="Paciente")
-
-        #LA LLAVE FORÁNEA DE INFORMES PERTENECE A OTRO GRUPO NO SE DONDE DEBO HACER LA TABLA
-    informes=models.ForeignKey(Informe, on_delete=models.CASCADE, verbose_name="informes")
 
